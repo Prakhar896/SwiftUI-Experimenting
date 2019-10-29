6140c0f2986315a33c7087f8719691d5e112a9b5
Prakhar Trivedi <prakhar0706@gmail.com>
2019-10-29 21:31:45 +0800

    Initial Commit

---
+++ SwiftUI Experimenting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Experimenting
+//
+//  Created by ssoeuser on 29/10/19.
+//  Copyright © 2019 ssoeuser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
